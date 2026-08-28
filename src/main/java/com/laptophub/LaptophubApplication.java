cdb481d29c51982ba6030c769f56fff84faf17b9
Nguyen Van Cuwr <nguyencu14503@gmail.com>
2026-08-28 22:20:02 +0700

    first commit

---
+++ src/main/java/com/laptophub/LaptophubApplication.java
@@ -0,0 +1,13 @@
+package com.laptophub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptophubApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(LaptophubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laptophub/LaptophubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laptophub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptophubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
